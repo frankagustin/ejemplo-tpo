52344ab72d2f4b8efb5fdf3a7e7fbea47d24871b
frankagustin <frank.agustin007@gmail.com>
2023-09-20 21:19:36 -0400

    first commit

---
+++ assets/js/script.js
@@ -0,0 +1,16 @@
+console.log("Hola mundo..");
+ let miVariable;
+
+ miVariable="Hello World."
+
+ console.log(miVariable)
+
+ //camelCase
+ let miVariable1;
+ var miVariable2;
+
+ //snake
+ let mi_variable1="2";
+ var mi_variable2=3;
+
+ console.log("Suma: "+mi_variable1+mi_variable2)
