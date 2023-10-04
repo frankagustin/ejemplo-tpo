eaf759430ad00fbc732797cc790a6753fa051971
frankagustin <frank.agustin007@gmail.com>
2023-10-04 20:32:53 -0300

    agregado branch funciones

---
+++ assets/js/scriptif.js
@@ -0,0 +1,8 @@
+let edad = 0;
+/* Number o parseint*/ 
+edad=(parseInt(prompt('Ingrese edad: ')));
+
+alert("Su edad es: "+ edad);
+
+(edad < 16) ? alert('Eres Menor a 16') : alert('Eres Mayor a 16');
+
